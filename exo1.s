8b50f6d9563d0866611ce5f0469ced26475667a5
megachonker <>
2022-03-14 20:12:40 +0100

    le bash test pass

--- exo1.s
+++ exo1.s
@@ -106,9 +106,68 @@ main_prodscal:
     //depiler tout le bordell
     reset
     
+main_racine:
+    push 25
+    call racine
+    reset
 
+
+//r = inf + (sup - inf)/2 ?
+moyenne:
+    sub %a,%b
+    div 2,%b
+    add %a,%b
+    rtn
 
 racine:
-    reset
-main_racine:
-    reset
+    push %b
+    ld 1,%a      //inf
+    ld [%sp+2],%b
+    
+    push %a //inf
+    push %b //sup
+
+    call moyenne
+
+    push %b //r
+
+    jmp while_loop
+
+    after_while:
+    pop %a
+    add 2,%sp
+    pop %b
+    rtn
+
+while_loop:
+
+    //(r+1)*(r+1)
+    add 1,%b
+    mul %b,%b
+    ld [%sp+5],%a//charge n
+    cmp %b,%a
+    jle while_if_2
+
+    //r*r > n
+    ld [%sp],%b//charge r
+    mul %b,%b  //r²
+    cmp %b,%a
+    jgt while_if_1
+
+
+    jmp after_while
+
+while_if_1:
+    ld [%sp],%a
+    st %a,[%sp+1]
+    jmp endif
+while_if_2:
+    ld [%sp],%a
+    st %a,[%sp+2]
+endif:
+    ld [%sp+2],%a
+    ld [%sp+1],%b
+    call moyenne
+    st %b,[%sp]    //metre b dans r
+    jmp while_loop
+
